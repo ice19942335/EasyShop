dbe4547f532b4a6f72c7c8de4912bb968e15d98e
ice19942335 <ice19942335@gmail.com>
2019-10-11 16:17:02 +0100

    Html/CSS + DB Migratin Initial_AddIdentity

--- Common/EasyShop.Domain/Entities/Identity/User.cs
+++ Common/EasyShop.Domain/Entities/Identity/User.cs
@@ -4,10 +4,11 @@ namespace EasyShop.Domain.Entities.Identity
 {
     public class User : IdentityUser
     {
-        public const string AdminUserName = "Administrator@server.com";
+        public const string AdminUserName = "Administrator@easyshop.com";
         public const string DefaultAdminPassword = "AdminPassword";
 
         public const string RoleAdmin = "Administrator";
         public const string RoleUser = "User";
     }
+
 }
--- UI/EasyShop.CP.UI/Startup.cs
+++ UI/EasyShop.CP.UI/Startup.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EasyShop.Clients.User;
+using EasyShop.DAL.Context;
 using EasyShop.Domain.Entities.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -17,12 +20,51 @@ namespace EasyShop.CP.UI
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddHttpContextAccessor();
             services.AddRazorPages();
+
+            services.AddDbContext<EasyShopContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<EasyShopContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 12;
+                options.Password.RequiredUniqueChars = 3;
+
+                options.Lockout.MaxFailedAccessAttempts = 30;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                options.Lockout.AllowedForNewUsers = true;
+
+                options.User.RequireUniqueEmail = true; //.....................................    Грабли
+
+                services.ConfigureApplicationCookie(options =>
+                {
+                    options.Cookie.HttpOnly = true;
+                    options.ExpireTimeSpan = TimeSpan.FromDays(7); //..........................    New in Core 3
+                    options.Cookie.MaxAge = TimeSpan.FromDays(7);
+
+                    options.LoginPath = "/Account/Login";
+                    options.LogoutPath = "/Account/Logout";
+                    options.AccessDeniedPath = "/Account/AccessDenied";
+
+                    options.SlidingExpiration = true; //.......................................    Change Session ID if being authorized
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +79,9 @@ namespace EasyShop.CP.UI
             app.UseDefaultFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication(); //Should be after "UseRouting" middleware
+            app.UseAuthorization();  //Should be after "UseRouting" middleware
 
             app.UseEndpoints(endpoints =>
             {
