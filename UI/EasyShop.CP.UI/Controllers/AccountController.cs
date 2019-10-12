618e761c2fb79c8c1355b46576890229d9bb96f0
ice19942335 <ice19942335@gmail.com>
2019-10-12 11:54:30 +0100

    Layouts infrastructure

---
+++ UI/EasyShop.CP.UI/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyShop.CP.UI.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register() => View();
+
+        public IActionResult Login() => View();
+
+        public IActionResult LogOut() => RedirectToAction("Index", "Home");
+
+        public IActionResult AccessDenied() => View();
+    }
+}
