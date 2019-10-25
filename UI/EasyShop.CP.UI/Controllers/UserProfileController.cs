38410f1e50c3da51591a0002a71c007223beeaf6
Aleksejs Birula <ice19942335@gmail.com>
2019-10-25 16:47:07 +0100

    UserProfile _LeftSideBar configuration + Button - Password reset request

--- UI/EasyShop.CP.UI/Controllers/UserProfileController.cs
+++ UI/EasyShop.CP.UI/Controllers/UserProfileController.cs
@@ -2,14 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EasyShop.Domain.Entities.Identity;
+using EasyShop.Domain.ViewModels.Account;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EasyShop.CP.UI.Controllers
 {
+    [Authorize]
     public class UserProfileController : Controller
     {
-        public IActionResult Index() => View();
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public IActionResult Security() => View();
+        public UserProfileController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index () => View();
+
+        public IActionResult PasswordResetRequest() => View();
+
+        public IActionResult EmailConfirmationStatus() => View();
     }
 }
