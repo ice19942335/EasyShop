afd7c9f051c3999d677b569b707a71329ac01c23
ice19942335 <ice19942335@gmail.com>
2019-12-13 11:09:40 +0000

    Added test Tariff and TariffOption + Migration in DAL

---
+++ Common/EasyShop.Domain/Entries/Tariff/TariffOption.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using EasyShop.Domain.Entries.Base;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace EasyShop.Domain.Entries.Tariff
+{
+    public class TariffOption
+    {
+        [Key]
+        [ForeignKey(nameof(TariffOf))]
+        public int TariffId { get; set; }
+
+        public string Description { get; set; }
+
+        public Tariff TariffOf { get; set; }
+    }
+}
--- Services/EasyShop.DAL/Context/EasyShopContext.cs
+++ Services/EasyShop.DAL/Context/EasyShopContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using EasyShop.Domain.Entries.Identity;
+using EasyShop.Domain.Entries.Tariff;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,6 +11,9 @@ namespace EasyShop.DAL.Context
     public class EasyShopContext : IdentityDbContext<AppUser>
     {
         //Tables
+        private DbSet<Tariff> Tariffs { get; set; }
+
+        private DbSet<TariffOption> TariffOptions { get; set; }
 
         //Constructor
         public EasyShopContext(DbContextOptions<EasyShopContext> options) : base(options) { }
