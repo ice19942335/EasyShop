bc63d4e32ad06f601726cbae1b6991107b7695e8
ice19942335 <ice19942335@gmail.com>
2019-12-18 16:16:42 +0000

    Added _ShopManagerLayout _LeftSideBar and ShopManagerController

---
+++ UI/EasyShop.CP.UI/Components/ControlPanel/SideBarShopsList/SideBarShopsListViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace EasyShop.CP.UI.Components.ControlPanel.SideBarShopsList
+{
+    public class SideBarShopsListViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var model = new Dictionary<string, int> {{"ShopName", 1}};
+            return View(model);
+        }
+    }
+}
---
+++ UI/EasyShop.CP.UI/Controllers/ShopManagerController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyShop.CP.UI.Controllers
+{
+    public class ShopManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
