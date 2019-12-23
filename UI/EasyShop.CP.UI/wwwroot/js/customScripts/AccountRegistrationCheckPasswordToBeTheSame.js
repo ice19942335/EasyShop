51c4be6cb6520637b07694696b8d914fb0e3ccd8
ice19942335 <ice19942335@gmail.com>
2019-12-23 14:02:50 +0000

    Added passwords inputs RED if passwords is not the same, fix in script string replace for registration form

---
+++ UI/EasyShop.CP.UI/wwwroot/js/customScripts/AccountRegistrationCheckPasswordToBeTheSame.js
@@ -0,0 +1,31 @@
+﻿'use strict';
+
+window.onload = function () {
+    document.getElementById("password").oninput = function fun(event) {
+        let formChildren = event.target.form.children;
+        let passInput1 = document.getElementById("password");
+        let passInput2 = document.getElementById("confirmPassword");
+
+        if (passInput1.value !== passInput2.value) {
+            passInput1.classList.add("bg-danger");
+            passInput2.classList.add("bg-danger");
+        } else {
+            passInput1.classList.remove("bg-danger");
+            passInput2.classList.remove("bg-danger");
+        }
+    }
+
+    document.getElementById("confirmPassword").oninput = function fun(event) {
+        let formChildren = event.target.form.children;
+        let passInput1 = document.getElementById("password");
+        let passInput2 = document.getElementById("confirmPassword");
+
+        if (passInput1.value !== passInput2.value) {
+            passInput1.classList.add("bg-danger");
+            passInput2.classList.add("bg-danger");
+        } else {
+            passInput1.classList.remove("bg-danger");
+            passInput2.classList.remove("bg-danger");
+        }
+    }
+}
--- UI/EasyShop.CP.UI/wwwroot/js/customScripts/RegistrationFormErrorListStringReplace.js
+++ UI/EasyShop.CP.UI/wwwroot/js/customScripts/RegistrationFormErrorListStringReplace.js
@@ -2,14 +2,19 @@
 substringReplace('User name', 'Email');
 
 function substringReplace(sub, newSub) {
-    let ul = document.querySelector('.stringReplace').children[0];
-
-    for (let node of ul.children) {
-        if (node !== undefined && node !== null) {
-            if (node.textContent.includes(sub)) {
-                node.textContent = node.textContent.replace(sub, newSub);
-                break;
+    let div = document.querySelector('.stringReplace');
+    if (div !== null && div !== undefined) {
+        let ul = div.children[0];
+        console.log(ul);
+        if (ul !== null && ul !== undefined) {
+            for (let li of ul.children) {
+                console.log(li);
+                if (li.textContent.includes(sub)) {
+                    li.textContent = li.textContent.replace(sub, newSub);
+                }
             }
         }
     }
 }
+
+
