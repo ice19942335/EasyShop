3d944b943793c5822c1b6cfb2fb30b20a85b0b1e
ice19942335 <ice19942335@gmail.com>
2019-12-24 09:22:32 +0000

    RustShop view menues edited + ShopSettingsActiveLink.js now set default active link if not Index

--- UI/EasyShop.CP.UI/wwwroot/js/customScripts/ShopSettingsActiveLink.js
+++ UI/EasyShop.CP.UI/wwwroot/js/customScripts/ShopSettingsActiveLink.js
@@ -3,16 +3,23 @@ selectActiveTopNavLink();
 selectActiveSideBarNavLink();
 
 function selectActiveTopNavLink() {
+
     let url = window.location.href;
     url = url.split('?')[0].split('/');
     let activeLinkId = `/${url[url.length - 2]}/${url[url.length - 1]}`;
 
     let ulNodes = document.querySelector('.profile-nav').children;
 
+    const setDefaultActiveLink = () => {
+        ulNodes[1].children[0].id = "shopSettingsActiveItem";
+    }
+
     for (let nodeLi of ulNodes) {
         if (nodeLi.children[0].pathname === activeLinkId) {
             nodeLi.children[0].id = "shopSettingsActiveItem";
             break;
+        } else {
+            setDefaultActiveLink();
         }
     }
 }
