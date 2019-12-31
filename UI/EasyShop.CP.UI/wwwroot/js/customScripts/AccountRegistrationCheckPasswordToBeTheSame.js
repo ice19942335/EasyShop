1d539307c74b774cee967b28eafaecb417a2aa8e
ice19942335 <ice19942335@gmail.com>
2019-12-31 20:56:25 +0000

    Added password comparison validation in Register View

--- UI/EasyShop.CP.UI/wwwroot/js/customScripts/AccountRegistrationCheckPasswordToBeTheSame.js
+++ UI/EasyShop.CP.UI/wwwroot/js/customScripts/AccountRegistrationCheckPasswordToBeTheSame.js
@@ -1,31 +1,24 @@
 ﻿'use strict';
 
-window.onload = function () {
-    document.getElementById("password").oninput = function fun(event) {
-        let formChildren = event.target.form.children;
-        let passInput1 = document.getElementById("password");
-        let passInput2 = document.getElementById("confirmPassword");
+document.getElementById('password').addEventListener("input", canPresRegisterButton);
+document.getElementById('confirmPassword').addEventListener("input", canPresRegisterButton);
 
-        if (passInput1.value !== passInput2.value) {
-            passInput1.classList.add("bg-danger");
-            passInput2.classList.add("bg-danger");
-        } else {
-            passInput1.classList.remove("bg-danger");
-            passInput2.classList.remove("bg-danger");
-        }
-    }
+let passInput1 = document.getElementById("password");
+let passInput2 = document.getElementById("confirmPassword");
 
-    document.getElementById("confirmPassword").oninput = function fun(event) {
-        let formChildren = event.target.form.children;
-        let passInput1 = document.getElementById("password");
-        let passInput2 = document.getElementById("confirmPassword");
+let passwordsHaveToBeTheSameDiv = document.getElementById('PasswordsHaveToBeTheSame');
+let registerButton = document.getElementById('register');
 
-        if (passInput1.value !== passInput2.value) {
-            passInput1.classList.add("bg-danger");
-            passInput2.classList.add("bg-danger");
-        } else {
-            passInput1.classList.remove("bg-danger");
-            passInput2.classList.remove("bg-danger");
-        }
+function canPresRegisterButton() {
+    if (passInput1.value !== passInput2.value) {
+        passInput1.classList.add('bg-danger');
+        passInput2.classList.add('bg-danger');
+        registerButton.setAttribute("disabled", "disabled");
+        passwordsHaveToBeTheSameDiv.style.display = 'block';
+    } else if (passInput1.value === passInput2.value) {
+        passInput1.classList.remove('bg-danger');
+        passInput2.classList.remove('bg-danger');
+        registerButton.removeAttribute("disabled");
+        passwordsHaveToBeTheSameDiv.style.display = 'none';
     }
 }
