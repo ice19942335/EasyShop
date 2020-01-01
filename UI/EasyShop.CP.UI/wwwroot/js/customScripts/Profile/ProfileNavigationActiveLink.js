59230314c6f41a2432fe2a0eab37b019985e969d
ice19942335 <ice19942335@gmail.com>
2020-01-01 11:14:59 +0000

    Added Profile side navidation active link highlight

---
+++ UI/EasyShop.CP.UI/wwwroot/js/customScripts/Profile/ProfileNavigationActiveLink.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+
+selectActiveSideNavigationLink();
+
+function selectActiveSideNavigationLink() {
+    let splittedUrl = window.location.href.split('?')[0].split('/');
+    let activeLinkId = `/${splittedUrl[splittedUrl.length - 2]}/${splittedUrl[splittedUrl.length - 1]}`;
+    console.log(activeLinkId);
+    console.log(splittedUrl);
+
+    let linkNodesWrap = document.getElementById('profileNavigation');
+
+    if (linkNodesWrap !== null && linkNodesWrap !== undefined) {
+        for (let link of linkNodesWrap.children) {
+            if (link.pathname === activeLinkId) {
+                link.classList.add('active');
+                break;
+            }
+        }
+    }
+}
