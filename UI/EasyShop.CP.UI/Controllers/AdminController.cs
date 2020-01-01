b7e13dcbc6d33ae7140480cb883cf75fc3ad372d
ice19942335 <ice19942335@gmail.com>
2020-01-01 11:45:10 +0000

    Added AdminPanelNavigationActiveLink + AdminTariffController was deleted all views was ported to AdminController

--- UI/EasyShop.CP.UI/Controllers/AdminController.cs
+++ UI/EasyShop.CP.UI/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using EasyShop.Domain.ViewModels.ControlPanel.Tariff;
 using EasyShop.Interfaces.Services.CP;
+using EasyShop.Interfaces.Services.CP.Tariff;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,6 +13,183 @@ namespace EasyShop.CP.UI.Controllers
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
+        private readonly ITariffService _tariffService;
+        private readonly ITariffOptionDescriptionService _tariffOptionDescriptionService;
+        private readonly ITariffOptionsService _tariffOptionsService;
+
+        public AdminController(
+            ITariffService tariffService,
+            ITariffOptionDescriptionService tariffOptionDescriptionService,
+            ITariffOptionsService tariffOptionsService)
+        {
+            _tariffService = tariffService;
+            _tariffOptionDescriptionService = tariffOptionDescriptionService;
+            _tariffOptionsService = tariffOptionsService;
+        }
+
+        public IActionResult TariffManager()
+        {
+            var tariffs = _tariffService.GetAll();
+            var tariffsOptions = _tariffOptionDescriptionService.GetAll();
+
+            var model = new TariffManagerViewModel
+            {
+                Tariffs = tariffs.Select(x => new EditTariffViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Price = x.Price,
+                    DaysActive = x.DaysActive,
+                    Description = x.Description
+                }),
+                TariffOptionDescriptions = tariffsOptions.Select(x => new TariffOptionDescriptionViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Description = x.Description
+                }),
+            };
+
+            return View(model);
+        }
+
+        public IActionResult SomethingWentWrong() => View();
+
+        #region Tariff
+        [HttpGet]
+        public IActionResult EditTariff(int? id)
+        {
+            if (id != null)
+            {
+                var model = _tariffService.GetById((int)id);
+                if (model is null)
+                    return View("SomethingWentWrong", "on getting tariff by id");
+
+                model.AllOptions = _tariffOptionDescriptionService.GetAll();
+                model.AssignedOptions = _tariffOptionsService.GetAllOptionsAssignedToATariffById((int)id);
+                return View(model);
+            }
+
+            return View("EditTariff", new EditTariffViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditTariff([FromForm] EditTariffViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.Values.SelectMany(x => x.Errors.Select(xx => xx.ErrorMessage)).ToList();
+                errors.ForEach(x => ModelState.AddModelError("", x));
+                return View(model);
+            }
+
+            if (model.Id is null)
+            {
+                await _tariffService.CreateAsync(model);
+                return View("TariffManager");
+            }
+
+            var tariffUpdated = await _tariffService.UpdateAsync(model);
+
+            if (tariffUpdated is null)
+                return View("SomethingWentWrong", "on updating existing tariff");
+
+
+            tariffUpdated.AllOptions = _tariffOptionDescriptionService.GetAll();
+            tariffUpdated.AssignedOptions = _tariffOptionsService.GetAllOptionsAssignedToATariffById((int)model.Id);
+            return View(tariffUpdated);
+        }
+
+        public async Task<IActionResult> DeleteTariff(int id)
+        {
+            var result = await _tariffService.DeleteByIdAsync(id);
+
+            if (result)
+                return View("TariffManager");
+
+            return View("SomethingWentWrong", "tariff deletion");
+        }
+        #endregion
+
+        #region TariffOptionDescription
+        [HttpGet]
+        public IActionResult EditTariffOptionDescription(int? id)
+        {
+            if (id != null)
+            {
+                var model = _tariffOptionDescriptionService.GetById((int)id);
+
+                if (model is null)
+                    return View("SomethingWentWrong", "on getting tariff option by id");
+
+                return View(model);
+            }
+
+            return View("EditTariffOptionDescription", new TariffOptionDescriptionViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditTariffOptionDescription([FromForm] TariffOptionDescriptionViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.Values.SelectMany(x => x.Errors.Select(xx => xx.ErrorMessage)).ToList();
+                errors.ForEach(x => ModelState.AddModelError("", x));
+                return View(model);
+            }
+
+            if (model.Id is null)
+            {
+                await _tariffOptionDescriptionService.CreateAsync(model);
+                return View("TariffManager");
+            }
+
+            var tariffUpdated = await _tariffOptionDescriptionService.UpdateAsync(model);
+
+            if (tariffUpdated is null)
+                return View("SomethingWentWrong", "on updating existing tariff option");
+
+            return View(tariffUpdated);
+        }
+
+        public async Task<IActionResult> DeleteTariffOptionDescription(int id)
+        {
+            var result = await _tariffOptionDescriptionService.DeleteByIdAsync(id);
+
+            if (result)
+                return View("TariffManager");
+
+            return View("SomethingWentWrong", "tariff option deletion");
+        }
+        #endregion
+
+        #region TarifOptionManipulation
+        [HttpGet]
+        public async Task<IActionResult> AddAnOptionToATariff(int tariffId, int optionId)
+        {
+            var result = await _tariffOptionsService.CreateAsync(tariffId, optionId);
+
+            var tariffModel = _tariffService.GetById(tariffId);
+
+            if (result is null)
+                return View("EditTariff", tariffModel);
+
+            return View("EditTariff", tariffModel);
+        }
+
+        public async Task<IActionResult> RemoveAnOptionFromATariff(int tariffId, int optionId)
+        {
+            var result = await _tariffOptionsService.DeleteAsync(tariffId, optionId);
+
+            var tariffModel = _tariffService.GetById(tariffId);
+
+            if (result is null)
+                return View("EditTariff", tariffModel);
+
+            return View("EditTariff", tariffModel);
+        }
+
         public IActionResult Index() => View();
+        #endregion
     }
 }
---
+++ UI/EasyShop.CP.UI/wwwroot/js/customScripts/AdminPanel/AdminPanelNavigationActiveLink.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+
+selectActiveSideNavigationLink();
+
+function selectActiveSideNavigationLink() {
+    let splittedUrl = window.location.href.split('?')[0].split('/');
+    let activeLinkId = `/${splittedUrl[splittedUrl.length - 2]}/${splittedUrl[splittedUrl.length - 1]}`;
+
+    let linkNodesWrap = document.getElementById('AdminNavigation');
+
+    if (linkNodesWrap !== null && linkNodesWrap !== undefined) {
+        for (let link of linkNodesWrap.children) {
+            console.log(link.pathname);
+            console.log(activeLinkId);
+
+            if (linkNodesWrap.children[0].classList.contains('index')) {
+                linkNodesWrap.children[0].classList.add('active');
+            }
+
+            if (link.pathname === activeLinkId) {
+                linkNodesWrap.children[0].classList.remove('active');
+                link.classList.add('active');
+                break;
+            }
+        }
+    }
+}
