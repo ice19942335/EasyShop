25347b2f087b78020e04b5389629c64515805ade
ice19942335 <ice19942335@gmail.com>
2020-01-12 14:42:44 +0000

    Added View: BugReportWasSuccessfullySent

--- UI/EasyShop.CP.UI/Controllers/ContactUsController.cs
+++ UI/EasyShop.CP.UI/Controllers/ContactUsController.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EasyShop.Domain.ViewModels.CP.ContactUs;
+using EasyShop.Interfaces.Services.CP.ContactUs;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EasyShop.CP.UI.Controllers
 {
     public class ContactUsController : Controller
     {
+        private readonly IBugReportService _bugReportService;
+
+        public ContactUsController(IBugReportService bugReportService)
+        {
+            _bugReportService = bugReportService;
+        }
+
         public IActionResult Index() => View();
 
         [HttpGet]
@@ -17,10 +25,23 @@ namespace EasyShop.CP.UI.Controllers
         [HttpPost]
         public IActionResult BugReport([FromForm] CreateBugReportViewModel model)
         {
-            var errors = ModelState.Values.SelectMany(x => x.Errors.Select(xx => xx.ErrorMessage)).ToList();
-            errors.ForEach(x => ModelState.AddModelError("", x));
-            return View(model);
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.Values.SelectMany(x => x.Errors.Select(xx => xx.ErrorMessage)).ToList();
+                errors.ForEach(x => ModelState.AddModelError("", x));
+                return View(model);
+            }
+
+            //var result = await _bugReportService.CreateBugReport(model);
+
+            //if (!result)
+            //    return RedirectToAction("SomethingWentWrong", "Home");
+
+            return View("BugReportWasSuccessfullySent");
+
         }
+
+        public IActionResult BugReportWasSuccessfullySent() => View();
 
         [HttpGet]
         public IActionResult GeneralSupport()
