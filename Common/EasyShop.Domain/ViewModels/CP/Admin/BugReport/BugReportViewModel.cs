7cd75cad33db02b457cee422814b519ea9d03396
ice19942335 <ice19942335@gmail.com>
2020-01-14 18:03:01 +0000

    Added ViewModels for Bug reports

---
+++ Common/EasyShop.Domain/ViewModels/CP/Admin/BugReport/BugReportViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyShop.Domain.ViewModels.CP.Admin.BugReport
+{
+    public class BugReportViewModel
+    {
+        public string Id { get; set; }
+
+        public string UserEmail { get; set; }
+
+        public BugReportCategoryViewModel BugReportCategoryViewModel { get; set; }
+
+        public ReportResponseViewModel ReportResponseViewModel { get; set; }
+
+        public string Title { get; set; }
+
+        public string Message { get; set; }
+
+        public string ImgUrl { get; set; }
+
+        public DateTime ReportedDateTime { get; set; }
+    }
+
+    public class ReportResponseViewModel
+    {
+        public string Id { get; set; }
+
+        public int Index { get; set; }
+
+        public string Description { get; set; }
+    }
+
+    public class BugReportCategoryViewModel
+    {
+        public string Id { get; set; }
+
+        public int Index { get; set; }
+
+        public string Description { get; set; }
+    }
+}
