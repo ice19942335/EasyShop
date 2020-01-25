22a1e09d38f3a67cfc278210b3f4d6e6d3c427f5
ice19942335 <ice19942335@gmail.com>
2020-01-25 12:30:12 +0000

    RustMultiTenantShop: implemented ExtensionInstaller

---
+++ MultiTenantShops/RustMultiTenantShop/Installers/MvcInstaller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RustMultiTenantShop.Installers
+{
+    public class MvcInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHttpContextAccessor();
+            services.AddControllers();
+            services.AddMvc();
+        }
+    }
+}
