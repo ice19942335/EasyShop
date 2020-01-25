06bb6058afea8694b63251f13a9bf7d71090ca77
ice19942335 <ice19942335@gmail.com>
2020-01-25 11:45:27 +0000

    Added HostResolutionStrategy and ITenantResolutionStrategy

---
+++ Services/EasyShop.Interfaces/MultiTenancy/ITenantResolutionStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyShop.Interfaces.MultiTenancy
+{
+    public interface ITenantResolutionStrategy
+    {
+        Task<string> GetTenantIdentifierAsync();
+    }
+}
---
+++ Services/EasyShop.Services/MultiTenancy/TenantResolution/HostResolutionStrategy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using EasyShop.Interfaces.MultiTenancy;
+using Microsoft.AspNetCore.Http;
+
+namespace EasyShop.Services.MultiTenancy.TenantResolution
+{
+    /// <summary>
+    /// Resolve the host to a tenant identifier
+    /// </summary>
+    public class HostResolutionStrategy : ITenantResolutionStrategy
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public HostResolutionStrategy(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        /// <summary>
+        /// Get the tenant identifier
+        /// </summary>
+        /// <returns></returns>
+        public async Task<string> GetTenantIdentifierAsync()
+        {
+            return await Task.FromResult(_httpContextAccessor.HttpContext.Request.Host.Host);
+        }
+    }
+}
