cb7c6418b075c610439fb2e27255fb27f5bcd76e
ice19942335 <ice19942335@gmail.com>
2020-02-10 22:51:02 +0000

    Rust.MultiTenant.Shop: Added TopUp view + Dynamic form

---
+++ MultiTenantShops/Rust.MultiTenant.Shop/wwwroot/js/top-up.js
@@ -0,0 +1,105 @@
+﻿'use strict';
+
+class TopUpForm {
+    constructor() {
+        this.topUpBtn;
+
+        this.dollarsInput;
+        this.dollarsInputValue;
+
+        this.topUpResult;
+
+        this.centsInput;
+        this.centsInputValue;
+
+        this.agree;
+
+        this.dollarsInputEventHandler = this.dollarsInputEventHandler.bind(this);
+        this.centsInputEventHandler = this.centsInputEventHandler.bind(this);
+        this.agreeClickEventHandler = this.agreeClickEventHandler.bind(this);
+    }
+    init() {
+        this.setVariables();
+        this.setEventListeners();
+    }
+    setVariables() {
+        this.topUpBtn = document.getElementById('top-up-btn');
+        this.dollarsInput = document.getElementById('dollars-input');
+        this.centsInput = document.getElementById('cents-input');
+        this.topUpResult = document.getElementById('topup-result');
+        this.agree = document.getElementById('agree');
+
+        this.dollarsInputValue = this.dollarsInput.value;
+        this.centsInputValue = this.centsInput.value;
+    }
+    setEventListeners() {
+        this.dollarsInput.addEventListener('input', this.dollarsInputEventHandler);
+        this.centsInput.addEventListener('input', this.centsInputEventHandler);
+        this.agree.addEventListener('click', this.agreeClickEventHandler);
+    }
+    dollarsInputEventHandler() {
+        const max = this.dollarsInput.getAttribute("max");
+
+        if (this.dollarsInput.value.length > max.length) {
+            this.dollarsInput.value = this.dollarsInputValue;
+            toastr.warning(`Please enter correct numbers`);
+        }
+
+        if (+this.dollarsInput.value > +max) {
+            this.dollarsInput.value = this.dollarsInputValue;
+            toastr.warning(`Max value for this field is: ${max}`);
+        }
+
+        this.dollarsInputValue = parseInt(this.dollarsInput.value, 10);
+
+        this.render();
+    }
+    centsInputEventHandler() {
+        const max = this.centsInput.getAttribute("max");
+
+        if (this.centsInput.value.length > 2) {
+            this.centsInput.value = this.centsInputValue;
+        }
+
+        this.centsInputValue = this.centsInput.value;
+
+        this.render();
+    }
+    agreeClickEventHandler() {
+        this.render();  
+    }
+    switchOnButton() {
+        this.topUpBtn.removeAttribute('disabled');
+    }
+    switchOffButton() {
+        this.topUpBtn.setAttribute('disabled', 'disabled');
+    }
+    render() {
+        if (this.centsInputValue !== '' && this.dollarsInputValue !== '' && !isNaN(this.centsInputValue) && !isNaN(this.dollarsInputValue)) {
+            this.switchOnButton();
+
+            if (this.centsInputValue.length === 1) {
+                this.centsInputValue += '0';
+            }
+
+            this.topUpResult.value = `${this.dollarsInputValue}.${(this.centsInputValue)}`;
+
+            if (this.agree.checked !== true) {
+                this.switchOffButton();
+            } else {
+                this.switchOnButton();
+            }
+            
+        } else {
+            this.switchOffButton();
+            this.topUpResult.value = '';
+        }
+    }
+}
+
+function initialize() {
+    const topUpForm = new TopUpForm();
+    topUpForm.init();
+}
+
+initialize();
